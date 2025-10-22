e72983ae91e69bbb224b5975314adacbfeaa4e06
unknown <juan.rrodriguez@labinfo.is.escuelaing.edu.co>
2025-10-22 09:02:14 -0500

    Scaffolding del  proxi y back

---
+++ Catalan/src/test/java/eci/edu/co/Catalan/CatalanApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.co.Catalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Proxi/src/test/java/eci/edu/co/Proxi/ProxiApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.co.Proxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
